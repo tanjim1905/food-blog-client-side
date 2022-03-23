22f311a0fcaeb6853fb12b6ea116dc1f7b5d9dc8
tanjim1905 <tanjimahmed1905@gmail.com>
2022-03-23 09:55:45 +0600

    install react, router, tailwind css and customize tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: "'Lora', serif",
+        playball: "'Playball', cursive"
+      },
+      colors: {
+        primary: "#EC9F2E",
+        secondary: "#8f9294"
+      }
+    },
+  },
+  plugins: [],
+}
